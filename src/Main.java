9e969f47f5c18b923bb0222c5389e7572e699023
PiGru08 <grucar77@gmail.com>
2024-09-18 11:27:50 +0200

    Brak modyfikatora dosetpu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Osoba osobaJas = new Osoba("Jaś", "Śliweczka", 7);
+
+        //niezgodne z regułą hermetyzacji
+        System.out.println(osobaJas.imie);
+        System.out.println(osobaJas.nazwisko);
+        //hermetyzacja
+        //powinno być tak żeby program główny nie mógł zmieniać własności klas
+
+
     }
 }
